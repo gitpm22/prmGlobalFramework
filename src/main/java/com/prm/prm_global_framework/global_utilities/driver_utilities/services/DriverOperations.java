d42ebb090524d61b18581fbf01aa399d3e246081
gitpm22 <preeti.mandavgade2208@gmail.com>
2020-09-14 12:04:12 +0530

    DriverOperations.java modified and TestDataRetrieval.java modified to test driver operations

--- src/main/java/com/prm/prm_global_framework/global_utilities/driver_utilities/services/DriverOperations.java
+++ src/main/java/com/prm/prm_global_framework/global_utilities/driver_utilities/services/DriverOperations.java
@@ -2,4 +2,52 @@ package com.prm.prm_global_framework.global_utilities.driver_utilities.services;
 
 public class DriverOperations {
 
+	public void loadChromeDriver(String chromeDriverKey, String chromeDriverValue) {
+
+
+		try {
+			if(chromeDriverKey.equals("browser.isChromeDriverEnable") && chromeDriverValue.equals("Yes")) {
+
+				System.out.println("Chrome Driver loaded successfully");
+			}
+
+		}
+		catch(NullPointerException e) {
+
+			System.out.println("Key and value is not matching");
+		}
+
+	}
+	public void loadGekoDriver(String gekoDriverKey, String gekoDriverValue) {
+		
+		try {
+			if(gekoDriverKey.equals("browser.isGekoDriverEnable") && gekoDriverValue.equals("Yes")) {
+
+				System.out.println("Geko Driver loaded successfully");
+			}
+
+		}
+		catch(NullPointerException e) {
+
+			System.out.println("Key and value is not matching");
+
+		}
+
+	}
+	public void loadIeDriver(String ieDriverKey, String ieDriverValue) {
+		try {
+			if(ieDriverKey.equals("browser.isInternetExplorerEnable") && ieDriverValue.equals("Yes")) {
+
+				System.out.println("IE Driver loaded successfully");
+
+			}
+		}
+		catch(NullPointerException e) {
+
+			System.out.println("Key and value is not matching");
+
+		}
+
+
+	}
 }
--- src/main/java/com/prm/prm_global_framework/test/services/TestDataRetrieval.java
+++ src/main/java/com/prm/prm_global_framework/test/services/TestDataRetrieval.java
@@ -3,8 +3,8 @@ package com.prm.prm_global_framework.test.services;
 import java.io.IOException;
 import java.util.List;
 
+import com.prm.prm_global_framework.global_utilities.driver_utilities.services.DriverOperations;
 import com.prm.prm_global_framework.global_utilities.propertiesfile_utilities.services.PropertiesFileOperations;
-import com.prm.prm_global_framework.global_utilities.propertiesfile_utilities.value_objects.ConfigurationFileVo;
 
 public class TestDataRetrieval {
 
@@ -17,12 +17,12 @@ public class TestDataRetrieval {
 			String propertiesFilePath = "selenium_global_framework_resources/browser_config.properties";
 
 			configFile.readPropertiesFile(propertiesFilePath);
+			
+			System.out.println("\n"+"-----------------------------------------------" + "\n");
 
 			List<String> keyValue = configFile.retrieveAllKeyValuePairsFromPropertiesFile(propertiesFilePath);		
 			List<String> onlyKeys = configFile.retrieveAllKeysFromPropertiesFile(propertiesFilePath);
 			List<String> onlyValue = configFile.retrieveAllKeyValuesFromPropertiesFile(propertiesFilePath);
-
-			System.out.println("\n"+"-----------------------------------------------" + "\n");
 
 			for(String e: keyValue) {
 
@@ -37,44 +37,26 @@ public class TestDataRetrieval {
 			System.out.println(onlyValue);
 			System.out.println("\n"+"-----------------------------------------------" + "\n");
 
-			ConfigurationFileVo file = new ConfigurationFileVo();
-
-
+			
 			int chromeIndex = 0;
 			int gekoIndex = 3;
 			int ieIndex = 6;
+			
+			String chromeKey = configFile.retrieveSingleKeyUsingIndex(keyValue, chromeIndex);
+			String chromeValue = configFile.retrieveSingleKeyValueUsingIndex(keyValue, chromeIndex);
 
-
-			String chromeKey = file.getKey(keyValue, chromeIndex);
-			String chromeValue = file.getValue(keyValue, chromeIndex);
-
-			String gekoKey = file.getKey(keyValue, gekoIndex);
-			String gekoValue = file.getValue(keyValue, gekoIndex);
-			String ieKey = file.getKey(keyValue, ieIndex);
-			String ieValue = file.getValue(keyValue, ieIndex);
-
-			System.out.println(chromeKey);
-			System.out.println("\n"+"-----------------------------------------------" + "\n");
-			System.out.println(chromeValue);
-			System.out.println("\n"+"-----------------------------------------------" + "\n");
-
-			if(chromeKey.equals("browser.isChromeDriverEnable") && chromeValue.equals("Yes")) {
-
-				System.out.println("Load Chrome Driver");
-
-			}
-
-			if(gekoKey.equals("browser.isGekoDriverEnable") && gekoValue.equals("Yes")) {
-
-				System.out.println("Load firefox Driver");
-
-			}
-
-			if(ieKey.equals("browser.isInternetExplorerEnable") && ieValue.equals("Yes")) {
-
-				System.out.println("Load IE Driver");
-
-			}
+			String gekoKey = configFile.retrieveSingleKeyUsingIndex(keyValue, gekoIndex);
+			String gekoValue = configFile.retrieveSingleKeyValueUsingIndex(keyValue, gekoIndex);
+			String ieKey = configFile.retrieveSingleKeyUsingIndex(keyValue, ieIndex);
+			String ieValue = configFile.retrieveSingleKeyValueUsingIndex(keyValue, ieIndex);
+			
+			// Test Driver Operations
+			
+			DriverOperations chromeDriver = new DriverOperations();
+			
+			chromeDriver.loadChromeDriver(chromeKey, chromeValue);
+			chromeDriver.loadGekoDriver(gekoKey, gekoValue);
+			chromeDriver.loadIeDriver(ieKey, ieValue);
 
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
